52eb0b116ffa509a6ddb84bf32db64b348cddd0a
Ivars Solovjovs <solovjovs.ivars@gmail.com>
2018-05-23 13:40:12 +0300

    Reading logs in correct format

--- graph.py
+++ graph.py
@@ -11,17 +11,24 @@ nodes = []
 G = nx.DiGraph()
 
 for file in files:
+    nodeId = ""
     f = open(file, 'r')
 
-    nodeId = os.path.basename(f.name) # Get nodeID from log file name
-    nodes.append(nodeId)
+    #nodeId = os.path.basename(f.name) # Get nodeID from log file name
 
     for line in f:
+        #values = line.rstrip()
+        #print line
         values = line.split(' ')
-        values = map(str.rstrip, values)
+        values[0] = values[0].replace('\r', '')
+        values[-1] = values[-1].replace('\n', '')
 
-        if line[0] == key:
-            G.add_edge(nodeId, values[1]),
+        if values[0] == key:
+            if not nodeId:
+                nodeId = values[1]
+                nodes.append(nodeId)
+
+            G.add_edge(nodeId, values[2])
 
     f.close()
 
