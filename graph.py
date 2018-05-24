8ed91fd0daf2f4f7f4c29d4f8283dc4266597c10
Ivars Solovjovs <solovjovs.ivars@gmail.com>
2018-05-24 14:16:07 +0300

    Layout and color updates

--- graph.py
+++ graph.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
 import glob
 import graph_node
 
@@ -7,6 +8,8 @@ key = "N"
 path = './log/*'
 files = glob.glob(path)
 nodes = []
+node_list = []
+edge_list = []
 G = nx.MultiDiGraph()
 
 for file in files:
@@ -36,9 +39,22 @@ for file in files:
     f.close()
 
 for node in nodes:
+    node_list.append(node.id)
     for neighbour in node.neighbours:
-        G.add_edge(node.id, neighbour)
+        node_list.append(neighbour)
+        edge_list.append((node.id, neighbour))
 
-nx.draw(G, with_labels=True, font_size=10, node_size=1000, linewidths=0)
+node_list = list(set(node_list))
+
+G.add_nodes_from(node_list)
+G.add_edges_from(edge_list)
+
+pos = nx.spring_layout(G, k=7*1/np.sqrt(len(G.nodes())), iterations=50)
+
+nx.draw(G, pos,
+        with_labels=True, font_size=10,
+        node_size=1000, node_color='#bcfcb8',
+        linewidths=0,
+        edge_color='#474747', edge_alpha=0.1)
 
 plt.show()
